d8d23ec6e9bbda2dce37db3f82e3a6a32612e6b0
Thanh Tuan <tuandtse243@gmail.com>
2023-07-10 16:00:03 +0700

    Edit URL backend

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-export const server = 'http://localhost:5000/api/v2'
-// export const server = 'https://s-store-backend.onrender.com/api/v2'
+// export const server = 'http://localhost:5000/api/v2'
+export const server = 'https://s-store-backend.onrender.com/api/v2'
 
 export const backend_url = "http://localhost:5000"
